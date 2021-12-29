3ffefa5d39a68a669693cd3b8301a4cbc94af3a6
Stanislav <sshchelokov@justcoded.com>
2021-12-29 19:00:00 +0200

    refectored project after code review

--- src/js/modules/burger-menu.js
+++ src/js/modules/burger-menu.js
@@ -1,31 +1,13 @@
 const burgermenu = () => {
-  const burger = document.querySelector('.header__burger'),
-    navMenu = document.querySelector('.header__nav'),
-    navLink = document.querySelectorAll('.header__list-link');
+  const burger = document.querySelector('.header__burger');
 
-  if (burger === null || navMenu === null || navLink.length === 0) {
+  if (burger === null) {
     return false;
   }
 
-  const addActiveClass = () => {
-    burger.classList.add('header__burger_active');
-    navMenu.classList.add('header__nav_active');
-    document.body.style.overflow = 'hidden';
-  };
-
-  const removeActiveClass = () => {
-    burger.classList.remove('header__burger_active');
-    navMenu.classList.remove('header__nav_active');
-    document.body.style.overflow = 'unset';
-  };
-
-  burger.addEventListener('click', () => {
-    if (burger.classList.contains('header__burger_active')) {
-      removeActiveClass();
-    } else {
-      addActiveClass();
-    }
-  });
+  burger.addEventListener('click', () =>
+    document.body.classList.toggle('nav-open')
+  );
 };
 
 export default burgermenu;
